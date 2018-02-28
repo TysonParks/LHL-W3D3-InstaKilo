59a139eff4380809ac3a3a8f72ef9bb756cb0882
tysonparks <typarks@gmail.com>
2018-02-28 13:01:41 -0800

    Added CollectionView with Reusable View and Cell. Created PhotoViewCell class. Added images

---
+++ InstaKilo/PhotoViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewCell.h
+//  InstaKilo
+//
+//  Created by Tyson Parks on 2/28/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewCell : UICollectionViewCell
+
+@end
---
+++ InstaKilo/PhotoViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoViewCell.m
+//  InstaKilo
+//
+//  Created by Tyson Parks on 2/28/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "PhotoViewCell.h"
+
+@implementation PhotoViewCell
+
+@end
